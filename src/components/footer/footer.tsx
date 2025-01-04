cf43cc78b959eb30bf6aa57db90f49204fb95a77
Daniel Holloway <daniel@vendettistudios.com>
2025-01-03 17:40:48 -0800

    add phone number

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -5,6 +5,7 @@ const Footer = () => {
     <footer className={styles.footer}>
       <p>© {new Date().getFullYear()} Cutler Storage. All rights reserved.</p>
       <p>Contact us: info@cutlerstorage.com</p>
+      <p>(858) 224-3598</p>
     </footer>
   );
 };
