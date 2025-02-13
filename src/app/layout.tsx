8c39afea8af04c19cfd5c1cba1450195eac1bca5
Daniel Holloway <daniel@vendettistudios.com>
2025-02-12 21:59:05 -0800

    microsoft clarity

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -14,7 +15,8 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "Outdoor Storage & Land for Lease in Cutler, CA | RV, Auto, & More",
-  description: "Secure outdoor storage for RVs, autos, and containers, plus 8 acres of prime land for lease in Cutler, CA. Flexible leasing options for farming or other ventures. Contact us today!",
+  description:
+    "Secure outdoor storage for RVs, autos, and containers, plus 8 acres of prime land for lease in Cutler, CA. Flexible leasing options for farming or other ventures. Contact us today!",
 };
 
 export default function RootLayout({
@@ -26,6 +28,14 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`(function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "q91pv8n1yy");`}
+    
+        </Script>
       </body>
     </html>
   );
