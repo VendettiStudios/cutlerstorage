c40a3788ecaea83c5d25f361fc802c49c3be56ce
Daniel Holloway <daniel@vendettistudios.com>
2025-03-02 14:33:21 -0800

    test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,9 +15,9 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Outdoor Storage & Land for Lease in Cutler, CA | RV, Auto, & More",
+  title: "Outdoor Storage & Land for Lease in Cutler, CA  | RV, Auto, & More",
   description:
-    "Secure outdoor storage for RVs, autos, and containers, plus 8 acres of prime land for lease in Cutler, CA. Flexible leasing options for farming or other ventures. Contact us today!",
+    "Secure outdoor storage for RVs, autos, and containers in Tulare County, plus 8 acres of prime land for lease in Cutler, CA. Flexible leasing options for farming or other ventures. Contact us today!",
 };
 
 export default function RootLayout({
